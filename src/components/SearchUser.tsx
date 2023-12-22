c8a95fccf9b992e573eba9e217bd239d07e1296d
İhsan Berk Özcan <59116996+ihsanberkozcan@users.noreply.github.com>
2023-12-22 21:13:20 +0300

    feat: admin page search user

--- src/components/SearchUser.tsx
+++ src/components/SearchUser.tsx
@@ -1,5 +1,6 @@
-import React, { Dispatch, SetStateAction, useState } from "react";
-
+import React, { Dispatch, SetStateAction, useEffect, useState } from "react";
+import { CiSearch } from "react-icons/ci";
+import { IoMdClose } from "react-icons/io";
 type SearchUser = {
   searchText: string;
   setSeacrhText: Dispatch<SetStateAction<string>>;
@@ -18,10 +19,27 @@ export default function SearchUser({
   userNumber: any;
   getAllUser: any;
 }) {
+  const [clear, setClear] = useState(false);
+  const [searchValue, setSearchValue] = useState("");
+  useEffect(() => {
+    getAllUser();
+    setClear(false);
+  }, [clear]);
   const handleKeyDown = (event: { key: string }) => {
     if (event.key === "Enter") {
       getAllUser();
     }
+  };
+
+  const handleClick = () => {
+    getAllUser();
+  };
+
+  const deleteSearch = async () => {
+    console.log("hehehe");
+    setSearchValue("");
+    setSeacrhText("");
+    setClear(true);
   };
 
   return (
@@ -29,13 +47,31 @@ export default function SearchUser({
       <div className="text-center font-medium text-3xl mt-4">All User</div>
       <div className="mb-5 w-full flex justify-between items-center">
         <div>{userNumber} User</div>
-        <input
-          type="text"
-          placeholder="Search User"
-          className="p-2 rounded-md"
-          onChange={(e) => setSeacrhText(e.target.value)}
-          onKeyDown={handleKeyDown}
-        />
+        <div className="flex items-center">
+          <input
+            type="text"
+            placeholder="Search User"
+            className="p-2 rounded-md mr-2"
+            onChange={(e) => {
+              setSeacrhText(e.target.value);
+              setSearchValue(e.target.value);
+            }}
+            value={searchValue}
+            onKeyDown={handleKeyDown}
+          />
+          <div
+            className="-ml-10 mr-3 hover:bg-slate-200 rounded-full p-1"
+            onClick={() => deleteSearch()}
+          >
+            <IoMdClose size={16} />
+          </div>
+          <button
+            className="bg-black p-3 h- ml-2 rounded-md text-white"
+            onClick={() => handleClick()}
+          >
+            <CiSearch />
+          </button>
+        </div>
       </div>
     </div>
   );
