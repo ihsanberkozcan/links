a3dd506bfb539a2a6aebd4663acebfa3aea7e723
İhsan Berk Özcan <59116996+ihsanberkozcan@users.noreply.github.com>
2023-12-30 11:10:44 +0300

    about us page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,8 +1,45 @@
-import Nav from '@/components/Nav'
-import React from 'react'
+import Nav from "@/components/Nav";
+import React from "react";
 
 export default function About() {
   return (
-    <div>  <Nav />About Page</div>
-  )
+    <div>
+      <Nav />
+      <div>
+        <div className="flex justify-center text-3xl my-16">
+          <h1 className="font-bold">About Us</h1>
+        </div>
+        <div className="flex justify-center">
+          <div className="w-5/6 text-center text-lg">
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Soluta
+              perspiciatis minus corrupti fugiat modi tempore cupiditate dolorem
+              quos dolores quasi ducimus similique quidem odio placeat nemo,
+              dolorum natus dolore repudiandae enim doloribus. Dolor, ab vitae
+              autem quibusdam eaque magnam rem unde voluptates animi qui
+              officiis, optio nemo quisquam odit iste velit. Labore odio,
+              distinctio error nemo eligendi ut non omnis vitae corporis,
+              doloribus tempora. Laboriosam illo nobis possimus, minima magnam,
+              voluptates aspernatur amet incidunt corporis atque fugit cum quo
+              vero maxime dolor eaque officia? Magnam reprehenderit, ipsa amet
+              recusandae aliquam iusto perferendis, adipisci doloremque ea
+              maxime consequuntur quis? Itaque, natus?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore,
+              ut. Nemo laborum voluptas non delectus inventore velit corporis
+              voluptatem fuga ex. Unde quis facilis incidunt id dolores at porro
+              non?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore,
+              ut. Nemo laborum voluptas non delectus inventore velit corporis
+              voluptatem fuga ex. Unde quis facilis incidunt id dolores at porro
+              non?
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
